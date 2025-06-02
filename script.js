a5f9c0b7380ea4b86fb9bad2578a6feb257b7963
Yusuf <iamrealyusuf@gmail.com>
2025-06-02 11:46:28 +0600

    updated

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-let icon = document.getElementById("toggleIcon");
+let toggleIcon = document.getElementById("toggleIcon");
 let calcBtn = document.getElementById("calculationButton");
 let mainBox = document.getElementById("mainBox");
+let outputField = document.getElementById("loveCount");
 
-// console.log(loveValue);
-icon.onclick = function () {
-  // Making dark mode
-  mainBox.classList.toggle("darkMode");
-  //   changing icon
-  if (icon.src.match("Moon")) {
-    icon.src = "Media/Sun.svg";
-  } else {
-    icon.src = "Media/Moon.svg";
-  }
-};
 //
-calcBtn.onclick = (e) => {
+let calculation = (e) => {
   e.preventDefault();
-  let yourName = document.getElementById("you").value.toLowerCase();
-  let partnerName = document.getElementById("partner").value.toLowerCase();
-  let outputField = document.getElementById("loveCount");
-  let loveValue = Math.floor(Math.random() * 70) + 20;
+  let yourName = document
+    .getElementById("you")
+    .value.toLowerCase()
+    .replace(/\s/g, "");
+  let partnerName = document
+    .getElementById("partner")
+    .value.toLowerCase()
+    .replace(/\s/g, "");
+  let nameLength = (yourName.length + partnerName.length) * 2;
+
   if (yourName === "" || partnerName === "") {
-    outputField.innerText = "Please, fill both of the field!";
+    outputField.innerText = "Fill both of the field, please!";
   } else if (
     (yourName.match("yusuf") && partnerName.match("eva")) ||
     (yourName.match("eva") && partnerName.match("yusuf"))
@@ -38,8 +34,49 @@ calcBtn.onclick = (e) => {
   ) {
     outputField.innerText = "100 %";
   } else if (yourName === partnerName) {
-    outputField.innerText = "Are you homo?";
+    outputField.innerText = "Use different name, please!";
+  } else if (nameLength <= 20) {
+    outputField.innerText = `${nameLength * 4}` + " %";
+  } else if (nameLength <= 50) {
+    outputField.innerText = `${Math.floor(nameLength * 1.5)}` + " %";
+  } else if (nameLength > 50 && nameLength < 90) {
+    outputField.innerText = `${nameLength}` + " %";
+  } else if (nameLength >= 90 && nameLength <= 99) {
+    outputField.innerText = `${Math.floor(nameLength / 2)}` + " %";
+  } else if (nameLength > 99) {
+    outputField.innerText = `${Math.ceil(Math.random() * 10 + 80)}` + " %";
+  }
+
+  if (outputField.innerText.includes("please")) {
+    outputField.style.color = "red";
   } else {
-    outputField.innerText = loveValue + "%";
+    outputField.style.color = mainBox.classList.contains("darkMode")
+      ? "white"
+      : "black";
+  }
+};
+
+//! Function Calling
+calcBtn.onclick = calculation;
+calcBtn.addEventListener("keydown", () => {
+  calculation;
+});
+
+toggleIcon.onclick = function () {
+  mainBox.classList.toggle("darkMode");
+
+  if (toggleIcon.classList.value.match("moon")) {
+    toggleIcon.classList.value = "fa-solid fa-sun";
+    toggleIcon.style.color = "white";
+  } else {
+    toggleIcon.classList.value = "fa-solid fa-moon";
+    toggleIcon.style.color = "black";
+  }
+  if (outputField.innerText.includes("please")) {
+    outputField.style.color = "red";
+  } else {
+    outputField.style.color = mainBox.classList.contains("darkMode")
+      ? "white"
+      : "black";
   }
 };
